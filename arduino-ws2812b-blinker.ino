ed7d41e75c9af56bc98ccf4d3892e4c9fd183ba7
sebastian merz <62221161+MerzSebastian@users.noreply.github.com>
2021-09-12 18:46:17 +0200

    Create arduino-ws2812b-blinker.ino

---
+++ arduino-ws2812b-blinker.ino
@@ -0,0 +1,103 @@
+#include <Adafruit_NeoPixel.h>
+#include <EEPROM.h>
+#define PIXEL_PIN 1
+#define PIXEL_COUNT 12
+
+Adafruit_NeoPixel pixels(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
+
+int offOnDelayOptions[] = {200,400,600,800,1000,1200,1400,1600,1800,2000,2200,2400};
+byte r = 255;
+byte g = 0;
+byte b = 0;
+
+int on_off_time = 0;
+int menuDelayThreshold = 1000;
+
+void setup() {
+  pinMode(2, INPUT_PULLUP);
+  pinMode(1, OUTPUT);
+  pixels.begin();
+  attachInterrupt(0, btnPressed, FALLING); //interupt 0 = pin 2
+  
+  r = EEPROM.read(100);
+  g = EEPROM.read(101);
+  b = EEPROM.read(102);
+  on_off_time = (EEPROM.read(103) << 8) + EEPROM.read(104);
+}
+
+void btnPressed()
+{
+  int startTime = millis();
+  int usedTime = 0;
+  while(digitalRead(2) == LOW){
+    usedTime = millis() - startTime;
+    if (usedTime >= menuDelayThreshold){
+      blinkLEDS(255,255,255,100,2);
+      byte selectedOption = 0;
+      while(digitalRead(2) == LOW && selectedOption < 12){
+        delay(400);
+        pixels.setPixelColor(selectedOption, pixels.Color(255,255,255));
+        pixels.show();
+        selectedOption += 1;
+      }
+      EEPROM.write(103, highByte(offOnDelayOptions[selectedOption-1]));
+      EEPROM.write(104, lowByte(offOnDelayOptions[selectedOption-1]));
+      on_off_time = offOnDelayOptions[selectedOption-1];
+      break;
+    }
+  }
+  if (usedTime < menuDelayThreshold){
+    changeColor();
+  }
+}
+
+
+void setLED(byte r, byte g, byte b){
+  for(int i=0; i<PIXEL_COUNT; i++) {
+    pixels.setPixelColor(i, pixels.Color(r,g,b));
+  }
+  delay(20);
+  pixels.show();
+}
+
+void blinkLEDS(byte r, byte g, byte b, int delayMs, int count){
+  for(int i=0; i<count;i++){
+      setLED(r,g,b);
+      delay(delayMs);
+      setLED(0,0,0);
+      delay(delayMs);
+  }
+}
+
+void changeColor(){
+  if(r == 255){
+    r = 0;
+    g = 255;
+    b = 0;
+  }
+  else if(g == 255){
+    r = 0;
+    g = 0;
+    b = 255;
+  }
+  else if(b == 255){
+    r = 255;
+    g = 0;
+    b = 0;
+  }
+  else{ //fallback for empty/wrong eeprom entry
+    r = 255;
+    g = 0;
+    b = 0;
+  }
+  EEPROM.write(100, r);
+  EEPROM.write(101, g);
+  EEPROM.write(102, b);
+}
+
+void loop() {
+  setLED(r,g,b);
+  delay(on_off_time);
+  setLED(0,0,0);
+  delay(on_off_time);
+}
